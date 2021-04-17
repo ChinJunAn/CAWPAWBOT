ba628eba4d3e76ed96766346269199409aec0ce8
cjunan <junan1599@gmail.com>
2021-04-17 23:05:13 +0800

    testing /add

--- testPoll.py
+++ testPoll.py
@@ -45,117 +45,125 @@ TOKEN = '1724307554:AAFAAOq5nkIM-XOPgfVnPB-KlYmYz7tKiIY'
 def start(update: Update, _: CallbackContext) -> None:
     """Inform user about what this bot can do"""
     update.message.reply_text(
-        'Please select /poll to get a Poll, /quiz to get a Quiz or /preview'
-        ' to generate a preview for your poll'
+        '/add to add flight members \n /remove to remove flight members \n /members show flight members \n /cawpaw <date> to begin recording parade state for specified date'
     )
 
+def add(update: Update, context: CallbackContext) -> None:
+	
+	def getCallsign(userinput):
+		callsign = "Callsign: " + userinput
+		return callsign
 
-def poll(update: Update, context: CallbackContext) -> None:
-    """Sends a predefined poll"""
-    questions = ["Good", "Really good", "Fantastic", "Great"]
-    message = context.bot.send_poll(
-        update.effective_chat.id,
-        "How are you?",
-        questions,
-        is_anonymous=False,
-        allows_multiple_answers=True,
-    )
-    # Save some info about the poll the bot_data for later use in receive_poll_answer
-    payload = {
-        message.poll.id: {
-            "questions": questions,
-            "message_id": message.message_id,
-            "chat_id": update.effective_chat.id,
-            "answers": 0,
-        }
-    }
-    context.bot_data.update(payload)
+	userinput = update.message.text
+	update.message.reply_text(getCallsign(userinput))
 
 
-def receive_poll_answer(update: Update, context: CallbackContext) -> None:
-    """Summarize a users poll vote"""
-    answer = update.poll_answer
-    poll_id = answer.poll_id
-    try:
-        questions = context.bot_data[poll_id]["questions"]
-    # this means this poll answer update is from an old poll, we can't do our answering then
-    except KeyError:
-        return
-    selected_options = answer.option_ids
-    answer_string = ""
-    for question_id in selected_options:
-        if question_id != selected_options[-1]:
-            answer_string += questions[question_id] + " and "
-        else:
-            answer_string += questions[question_id]
-    context.bot.send_message(
-        context.bot_data[poll_id]["chat_id"],
-        f"{update.effective_user.mention_html()} feels {answer_string}!",
-        parse_mode=ParseMode.HTML,
-    )
-    context.bot_data[poll_id]["answers"] += 1
-    # Close poll after three participants voted
-    if context.bot_data[poll_id]["answers"] == 3:
-        context.bot.stop_poll(
-            context.bot_data[poll_id]["chat_id"], context.bot_data[poll_id]["message_id"]
-        )
+# def poll(update: Update, context: CallbackContext) -> None:
+#     """Sends a predefined poll"""
+#     questions = ["Good", "Really good", "Fantastic", "Great"]
+#     message = context.bot.send_poll(
+#         update.effective_chat.id,
+#         "How are you?",
+#         questions,
+#         is_anonymous=False,
+#         allows_multiple_answers=True,
+#     )
+#     # Save some info about the poll the bot_data for later use in receive_poll_answer
+#     payload = {
+#         message.poll.id: {
+#             "questions": questions,
+#             "message_id": message.message_id,
+#             "chat_id": update.effective_chat.id,
+#             "answers": 0,
+#         }
+#     }
+#     context.bot_data.update(payload)
 
 
-def quiz(update: Update, context: CallbackContext) -> None:
-    """Send a predefined poll"""
-    questions = ["1", "2", "4", "20"]
-    message = update.effective_message.reply_poll(
-        "How many eggs do you need for a cake?", questions, type=Poll.QUIZ, correct_option_id=2
-    )
-    # Save some info about the poll the bot_data for later use in receive_quiz_answer
-    payload = {
-        message.poll.id: {"chat_id": update.effective_chat.id, "message_id": message.message_id}
-    }
-    context.bot_data.update(payload)
+# def receive_poll_answer(update: Update, context: CallbackContext) -> None:
+#     """Summarize a users poll vote"""
+#     answer = update.poll_answer
+#     poll_id = answer.poll_id
+#     try:
+#         questions = context.bot_data[poll_id]["questions"]
+#     # this means this poll answer update is from an old poll, we can't do our answering then
+#     except KeyError:
+#         return
+#     selected_options = answer.option_ids
+#     answer_string = ""
+#     for question_id in selected_options:
+#         if question_id != selected_options[-1]:
+#             answer_string += questions[question_id] + " and "
+#         else:
+#             answer_string += questions[question_id]
+#     context.bot.send_message(
+#         context.bot_data[poll_id]["chat_id"],
+#         f"{update.effective_user.mention_html()} feels {answer_string}!",
+#         parse_mode=ParseMode.HTML,
+#     )
+#     context.bot_data[poll_id]["answers"] += 1
+#     # Close poll after three participants voted
+#     if context.bot_data[poll_id]["answers"] == 3:
+#         context.bot.stop_poll(
+#             context.bot_data[poll_id]["chat_id"], context.bot_data[poll_id]["message_id"]
+#         )
 
 
-def receive_quiz_answer(update: Update, context: CallbackContext) -> None:
-    """Close quiz after three participants took it"""
-    # the bot can receive closed poll updates we don't care about
-    if update.poll.is_closed:
-        return
-    if update.poll.total_voter_count == 3:
-        try:
-            quiz_data = context.bot_data[update.poll.id]
-        # this means this poll answer update is from an old poll, we can't stop it then
-        except KeyError:
-            return
-        context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
+# def quiz(update: Update, context: CallbackContext) -> None:
+#     """Send a predefined poll"""
+#     questions = ["1", "2", "4", "20"]
+#     message = update.effective_message.reply_poll(
+#         "How many eggs do you need for a cake?", questions, type=Poll.QUIZ, correct_option_id=2
+#     )
+#     # Save some info about the poll the bot_data for later use in receive_quiz_answer
+#     payload = {
+#         message.poll.id: {"chat_id": update.effective_chat.id, "message_id": message.message_id}
+#     }
+#     context.bot_data.update(payload)
 
 
-def preview(update: Update, _: CallbackContext) -> None:
-    """Ask user to create a poll and display a preview of it"""
-    # using this without a type lets the user chooses what he wants (quiz or poll)
-    button = [[KeyboardButton("Press me!", request_poll=KeyboardButtonPollType())]]
-    message = "Press the button to let the bot generate a preview for your poll"
-    # using one_time_keyboard to hide the keyboard
-    update.effective_message.reply_text(
-        message, reply_markup=ReplyKeyboardMarkup(button, one_time_keyboard=True)
-    )
+# def receive_quiz_answer(update: Update, context: CallbackContext) -> None:
+#     """Close quiz after three participants took it"""
+#     # the bot can receive closed poll updates we don't care about
+#     if update.poll.is_closed:
+#         return
+#     if update.poll.total_voter_count == 3:
+#         try:
+#             quiz_data = context.bot_data[update.poll.id]
+#         # this means this poll answer update is from an old poll, we can't stop it then
+#         except KeyError:
+#             return
+#         context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
 
 
-def receive_poll(update: Update, _: CallbackContext) -> None:
-    """On receiving polls, reply to it by a closed poll copying the received poll"""
-    actual_poll = update.effective_message.poll
-    # Only need to set the question and options, since all other parameters don't matter for
-    # a closed poll
-    update.effective_message.reply_poll(
-        question=actual_poll.question,
-        options=[o.text for o in actual_poll.options],
-        # with is_closed true, the poll/quiz is immediately closed
-        is_closed=True,
-        reply_markup=ReplyKeyboardRemove(),
-    )
+# def preview(update: Update, _: CallbackContext) -> None:
+#     """Ask user to create a poll and display a preview of it"""
+#     # using this without a type lets the user chooses what he wants (quiz or poll)
+#     button = [[KeyboardButton("Press me!", request_poll=KeyboardButtonPollType())]]
+#     message = "Press the button to let the bot generate a preview for your poll"
+#     # using one_time_keyboard to hide the keyboard
+#     update.effective_message.reply_text(
+#         message, reply_markup=ReplyKeyboardMarkup(button, one_time_keyboard=True)
+#     )
+
+
+# def receive_poll(update: Update, _: CallbackContext) -> None:
+#     """On receiving polls, reply to it by a closed poll copying the received poll"""
+#     actual_poll = update.effective_message.poll
+#     # Only need to set the question and options, since all other parameters don't matter for
+#     # a closed poll
+#     update.effective_message.reply_poll(
+#         question=actual_poll.question,
+#         options=[o.text for o in actual_poll.options],
+#         # with is_closed true, the poll/quiz is immediately closed
+#         is_closed=True,
+#         reply_markup=ReplyKeyboardRemove(),
+#     )
 
 
 def help_handler(update: Update, _: CallbackContext) -> None:
     """Display a help message"""
-    update.message.reply_text("Use /quiz, /poll or /preview to test this bot.")
+    update.message.reply_text('/initialise to add update flight members \n /cawpaw <date> to begin recording parade state for specified date')
 
 
 def main() -> None:
@@ -163,13 +171,16 @@ def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('poll', poll))
-    dispatcher.add_handler(PollAnswerHandler(receive_poll_answer))
-    dispatcher.add_handler(CommandHandler('quiz', quiz))
-    dispatcher.add_handler(PollHandler(receive_quiz_answer))
-    dispatcher.add_handler(CommandHandler('preview', preview))
-    dispatcher.add_handler(MessageHandler(Filters.poll, receive_poll))
-    dispatcher.add_handler(CommandHandler('help', help_handler))
+    dispatcher.add_handler(MessageHandler(Filters.text,add))
+    # dispatcher.add_handler(CommandHandler('add', add))
+
+
+    # dispatcher.add_handler(PollAnswerHandler(receive_poll_answer))
+    # dispatcher.add_handler(CommandHandler('quiz', quiz))
+    # dispatcher.add_handler(PollHandler(receive_quiz_answer))
+    # dispatcher.add_handler(CommandHandler('preview', preview))
+    # dispatcher.add_handler(MessageHandler(Filters.poll, receive_poll))
+    # dispatcher.add_handler(CommandHandler('help', help_handler))
 
     # Start the Bot
     #updater.start_polling()

a454d64c0208e6cc4356bef064e618145c8bb759
cjunan <junan1599@gmail.com>
2021-04-18 10:46:55 +0800

    adding /cawpaw function

--- testPoll.py
+++ testPoll.py
@@ -69,22 +69,26 @@ def cawpaw(update: Update, context: CallbackContext) -> None:
 	date = update.message.text[8:0]
 
 	keyboard = [
+		#date
 		[InlineKeyboardButton(date, callback_data ='0')],
-
-		for x in context.chat_data["flightMembers"]:
-			#name
-			[InlineKeyboardButton(context.chat_data["flightMembers"][x], callback_data ='0')],
-			#options
-			[
-			InlineKeyboardButton("AM", callback_data='1'),
-			InlineKeyboardButton("PM", callback_data='1'),
-			InlineKeyboardButton("In", callback_data='1'),
-			InlineKeyboardButton("OFF", callback_data='2'),
-			InlineKeyboardButton("LVE", callback_data='1'),
-			InlineKeyboardButton("CSE", callback_data='1'),
-			InlineKeyboardButton("OS", callback_data='1'),
-			],
 	]
+
+	for x in context.chat_data["flightMembers"]:
+		keyboard[date].append(
+		#name
+		[InlineKeyboardButton(context.chat_data["flightMembers"][x], callback_data ='0')],
+		#options
+		[
+		InlineKeyboardButton("AM", callback_data='1'),
+		InlineKeyboardButton("PM", callback_data='1'),
+		InlineKeyboardButton("In", callback_data='1'),
+		InlineKeyboardButton("OFF", callback_data='1'),
+		InlineKeyboardButton("LVE", callback_data='1'),
+		InlineKeyboardButton("CSE", callback_data='1'),
+		InlineKeyboardButton("OS", callback_data='1'),
+		],
+		)
+
 	reply_markup = InlineKeyboardMarkup(keyboard)
 	update.message.reply_text('Please choose:', reply_markup=reply_markup)
 

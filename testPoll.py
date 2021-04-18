0c43ff29ae65df4843960630f4d3bb2d3999e135
cjunan <junan1599@gmail.com>
2021-04-18 18:27:42 +0800

    test check boxes

--- testPoll.py
+++ testPoll.py
@@ -101,25 +101,22 @@ def members(update: Update, context: CallbackContext) -> None:
 def cawpaw(update: Update, context: CallbackContext) -> None:
 
 	date = update.message.text[8:]
-	tempKeyboard.clear()
-	tempKeyboard.append(keyboard)
-	reply_markup = InlineKeyboardMarkup(tempKeyboard)
+	reply_markup = InlineKeyboardMarkup(keyboard)
 	update.message.reply_text('Parade state for *__'+date+'__*', reply_markup=reply_markup, parse_mode='MarkdownV2')
 
 def addCheck(update: Update, _: CallbackContext) -> None:
 
 	query = update.callback_query
 	query.answer()
-
 	target = query.data.split(',')
 
 	#check if callback data returns with check or no check
 	#if have check, add check 
 	#if no have check, remove check
 
-	tempKeyboard[int(target[0])][int(target[1])] = InlineKeyboardButton(str(target[2]),callback_data='none')
+	keyboard[int(target[0])][int(target[1])] = InlineKeyboardButton(str(target[2]),callback_data='none')
 
-	reply_markup = InlineKeyboardMarkup(tempKeyboard)
+	reply_markup = InlineKeyboardMarkup(keyboard)
 	#query.edit_message_text(text= 'Parade state for *__'+date+'__*', reply_markup= reply_markup)
 	#query.edit_message_text(text=target)
 	query.edit_message_text(text= target, reply_markup=reply_markup, parse_mode='MarkdownV2')
